473f13819ee8e1647488457a2caa4eb24eb6b1a0
Jeevitha0725 <s.jeevitha234@gmail.com>
2024-12-26 20:16:51 +0530

    regex

--- converted_c.c
+++ converted_c.c
@@ -1,23 +1,6 @@
 #include <stdio.h>
 
 int main() {
-  int num, temp, digit, sum = 0;
-
-  printf("Enter a number: ");
-  scanf("%d", &num);
-
-  temp = num;
-  while (temp > 0) {
-    digit = temp % 10;
-    sum += digit * digit * digit;
-    temp /= 10;
-  }
-
-  if (num == sum) {
-    printf("%d is an Armstrong number\n", num);
-  } else {
-    printf("%d is not an Armstrong number\n", num);
-  }
-
+  printf("Hello world\n");
   return 0;
 }
--- converted_py.py
+++ converted_py.py
@@ -1,2 +1,1 @@
-var = 43
-print(var)
+print("Hello world")
